c1c8b51f154c8c74a26ec93204d838b019491f15
Tyler <m0bley@icloud.com>
2023-11-06 14:40:35 +0000

    Created model for DB

---
+++ server/models/game.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const gameSchema = new Schema({
+    title: String,
+    year: Number,
+    developer: String,
+    publisher: String,
+    ign_rating: Number,
+    description: String,
+    genre: Array,
+    recommended: Boolean,
+});
+
+const Game = mongoose.model("Game", gameSchema);
+
+module.exports = Game;
